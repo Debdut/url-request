966d3306c90b3d6388e9a8de16e1d19ea50d9c8a
Debdut Karmakar <iawaiponly@gmail.com>
2020-07-14 02:31:51 +0530

    Fork Example

---
+++ examples/fork.js
@@ -0,0 +1,40 @@
+const Url = require('../lib')
+
+const Api = new Url('https://my-json-server.typicode.com')
+  .go('typicode', 'demo')
+
+const Posts = Api
+  .fork()
+  .go('posts')
+
+const Comments = Api
+  .fork()
+  .go('comments')
+
+const Post = (id) => Posts
+  .fork()
+  .go(id)
+
+const apis = [ Posts, Post(2), Comments ]
+
+for (let index = 0; index < apis.length; index++) {
+  const api = apis[index]
+  api
+    .get()
+    .then(response => console.log(response, '\n'))
+    .catch(err => console.log(err))
+  
+}
+
+// [
+//   { id: 1, title: 'Post 1' },
+//   { id: 2, title: 'Post 2' },
+//   { id: 3, title: 'Post 3' }
+// ]
+
+// { id: 2, title: 'Post 2' }
+
+// [
+//   { id: 1, body: 'some comment', postId: 1 },
+//   { id: 2, body: 'some comment', postId: 1 }
+// ]
